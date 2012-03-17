14a62ceaefbe2d1fa15ae87078832ea5be0e4224
Robert Meijers <robert.meijers@gmail.com>
2012-03-17 21:20:10 +0100

    Override MeeTvEpgQuery::channel property with MeeTvChannel type

--- meetvepgquery.cpp
+++ meetvepgquery.cpp
@@ -3,13 +3,27 @@
 #include "meetvhtsp.h"
 
 MeeTvEpgQuery::MeeTvEpgQuery(QObject *parent) :
-    QHtspEpgQuery(MeeTvHtsp::instance(), parent), m_eventModel(0)
+    QHtspEpgQuery(MeeTvHtsp::instance(), parent), m_channel(0), m_eventModel(0)
 {
 }
 
 MeeTvEpgQuery::MeeTvEpgQuery(const QHtspEpgQuery& epgQuery, QObject *parent) :
-    QHtspEpgQuery(epgQuery, parent), m_eventModel(0)
+    QHtspEpgQuery(epgQuery, parent), m_channel(0), m_eventModel(0)
 {
+}
+
+MeeTvChannel *MeeTvEpgQuery::channel()
+{
+    QHtspChannel *channel = QHtspEpgQuery::channel();
+    if(!m_channel || m_channel->id() != channel->id())
+    {
+        if(!channel)
+            setChannel(0);
+        else
+            setChannel(new MeeTvChannel(*channel, this));
+    }
+
+    return m_channel;
 }
 
 MeeTvEventModel *MeeTvEpgQuery::eventsModel()
@@ -18,4 +32,13 @@ MeeTvEventModel *MeeTvEpgQuery::eventsModel()
         m_eventModel = new MeeTvEventModel(events());
 
     return m_eventModel;
+}
+
+void MeeTvEpgQuery::setChannel(MeeTvChannel *channel)
+{
+    if(m_channel)
+        delete m_channel;
+
+    m_channel = channel;
+    QHtspEpgQuery::setChannel(channel);
 }
--- meetvepgquery.h
+++ meetvepgquery.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 
+#include "meetvchannel.h"
 #include "meetveventmodel.h"
 #include "QHtsp/qhtspepgquery.h"
 
@@ -10,13 +11,18 @@ class MeeTvEpgQuery : public QHtspEpgQuery
 {
     Q_OBJECT
     Q_PROPERTY(MeeTvEventModel *eventsModel READ eventsModel)
+    Q_PROPERTY(MeeTvChannel *channel READ channel WRITE setChannel)
 public:
     explicit MeeTvEpgQuery(QObject *parent = 0);
     MeeTvEpgQuery(const QHtspEpgQuery& epgQuery, QObject *parent = 0);
 
+    MeeTvChannel *channel();
     MeeTvEventModel *eventsModel();
 
+    void setChannel(MeeTvChannel *channel);
+
 private:
+    MeeTvChannel *m_channel;
     MeeTvEventModel *m_eventModel;
     
 };

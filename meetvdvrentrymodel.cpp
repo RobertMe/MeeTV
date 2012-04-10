d2303c54e3d9860d9384ede21e642edd910d40d6
Robert Meijers <robert.meijers@gmail.com>
2012-04-10 19:11:37 +0200

    dvrentrymodel: implement meetvmodel

--- meetvdvrentrymodel.cpp
+++ meetvdvrentrymodel.cpp
@@ -5,7 +5,17 @@ MeeTvDvrEntryModel::MeeTvDvrEntryModel(QHtspDvrEntryList *dvrEntries) :
 {
 }
 
+MeeTvDvrEntry *MeeTvDvrEntryModel::get(int index)
+{
+    return getDvrEntryByIndex(index);
+}
+
 MeeTvDvrEntry *MeeTvDvrEntryModel::getDvrEntryById(int id)
 {
     return new MeeTvDvrEntry(*QHtspDvrEntryModel::getDvrEntryById(id));
+}
+
+MeeTvDvrEntry *MeeTvDvrEntryModel::getDvrEntryByIndex(int index)
+{
+    return new MeeTvDvrEntry(*QHtspDvrEntryModel::getDvrEntryByIndex(index));
 }
--- meetvdvrentrymodel.h
+++ meetvdvrentrymodel.h
@@ -4,17 +4,19 @@
 #include <QObject>
 
 #include "meetvdvrentry.h"
+#include "meetvmodel.h"
 #include "QHtsp/qhtspdvrentrylist.h"
 #include "QHtsp/qhtspdvrentrymodel.h"
 
-class MeeTvDvrEntryModel : public QHtspDvrEntryModel
+class MeeTvDvrEntryModel : public QHtspDvrEntryModel, public MeeTvModel
 {
     Q_OBJECT
 public:
     explicit MeeTvDvrEntryModel(QHtspDvrEntryList *dvrEntries);
     
+    Q_INVOKABLE MeeTvDvrEntry *get(int index);
     Q_INVOKABLE MeeTvDvrEntry *getDvrEntryById(int id);
-    
+    Q_INVOKABLE MeeTvDvrEntry *getDvrEntryByIndex(int index);
 };
 
 #endif // MEETVDVRENTRYMODEL_H
